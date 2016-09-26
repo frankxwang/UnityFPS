d5ba2ac9d848a83ffdfc5c518c5b0983029fe9b3
thegithubgeek <wfrankw9@gmail.com>
2016-09-25 18:17:07 -0700

    lots oh stuffs

---
+++ CS-GO/Assets/Health.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+public class Health : NetworkBehaviour 
+{
+	public const int maxHealth = 100;
+	[SyncVar]
+	public int currentHealth = maxHealth;
+	public Texture t;
+	public Texture cross;
+	public GameObject[] spawn;
+	public void TakeDamage(int amount)
+	{
+		currentHealth -= amount;
+		if (currentHealth <= 0)
+		{
+			currentHealth = maxHealth;
+			RpcRespawn ();
+		}
+	}
+	public void OnGUI(){
+		GUI.Box (new Rect(Screen.width/2 - 5, Screen.height/2 - 5, 10, 10), cross);
+		GUI.Box (new Rect(0, 0, 200, 20), t);
+		GUI.Box (new Rect(0, 0, currentHealth/maxHealth * 200, 20), t);
+		GUI.Box (new Rect(0, 20, 200, 20), "Health: " + currentHealth);
+	}
+	[ClientRpc]
+	void RpcRespawn()
+	{
+		if (isLocalPlayer)
+		{
+			// move back to zero location
+			transform.position = spawn[Random.Range(0, spawn.Length)].transform.position;
+		}
+	}
+}
---
+++ CS-GO/Assets/PlayerController.cs
@@ -0,0 +1,109 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+public class PlayerController : NetworkBehaviour
+{
+	public GameObject cam;
+	public GameObject camera;
+	public GameObject smoke;
+	public GameObject hitParticles;
+	public GameObject bulletPrefab;
+	public Transform bulletSpawn;
+	public float rate = 0.1f;
+	private float lastShot = 0;
+	private Rigidbody rb;
+	void Start()
+	{
+		rb = GetComponent<Rigidbody> ();
+		if (!isLocalPlayer)
+		{
+			camera.SetActive(false);
+			return;
+		}
+		Cursor.lockState = CursorLockMode.Locked;
+		Cursor.visible = false;
+	}
+	void Update()
+	{
+		if (Input.GetKey (KeyCode.Escape)) {
+			Cursor.lockState = CursorLockMode.None;
+			Cursor.visible = true;
+		} else if (Input.GetKey ("e")) {
+			Cursor.lockState = CursorLockMode.Locked;
+			Cursor.visible = false;
+		}
+			
+		float mul = 1;
+		if (Input.GetKey(KeyCode.LeftShift))
+		{
+			mul = 2;
+		}
+		if (!isLocalPlayer)
+		{
+			return;
+		}
+		var x = Input.GetAxis("Horizontal") * Time.deltaTime * 3.0f;
+		var z = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
+
+		transform.Rotate(0, Input.GetAxis("Mouse X") * Time.deltaTime*150, 0);
+		cam.transform.Rotate(Input.GetAxis("Mouse Y") * -1 *Time.deltaTime*150, 0, 0);
+		rb.rotation = Quaternion.Euler(0, rb.rotation.eulerAngles.y, 0);
+		transform.Translate(x*mul, 0, z*mul);
+		if (Input.GetMouseButton(0) && rate + lastShot < Time.time)
+		{
+			lastShot = Time.time;
+			CmdFire();
+		}
+		if(Input.GetKeyDown(KeyCode.Space) && grounded()){
+			rb.AddForce(0, 250, 0);
+		}
+	}
+	[Command]
+	void CmdFire()
+	{
+		//smoke
+		GameObject smokeParticles = (GameObject)Instantiate(smoke, bulletSpawn.position, Quaternion.identity);
+		ParticleSystem[] psa = smokeParticles.GetComponentsInChildren<ParticleSystem> ();
+		foreach(ParticleSystem ps in psa){
+			ps.Emit(2);
+		}
+		NetworkServer.Spawn (smokeParticles);
+		// Create the Bullet from the Bullet Prefab
+		var bullet = (GameObject)Instantiate(
+			bulletPrefab,
+			bulletSpawn.position,
+			bulletSpawn.rotation);
+
+		// Add velocity to the bullet
+		bullet.GetComponent<Rigidbody>().velocity = cam.transform.forward * 600;
+
+		NetworkServer.Spawn (bullet);
+
+		// Destroy the bullet after 2 seconds
+		Destroy(bullet, 2.0f);
+
+		RaycastHit hit;
+		if(Physics.Raycast(cam.transform.position, cam.transform.forward, out hit, 9999999)){
+			var health = hit.rigidbody.GetComponent<Health>();
+			if (health  != null)
+			{
+				//hit particles
+				GameObject particles = (GameObject)Instantiate(hitParticles, hit.point, Quaternion.identity);
+				particles.GetComponent<ParticleSystem> ().Emit(10);
+				NetworkServer.Spawn (particles);
+				health.TakeDamage(10);
+			}
+
+		}
+	}
+	bool grounded(){
+		RaycastHit hit;
+		if(Physics.Raycast(transform.position, -Vector3.up, out hit)){
+			Debug.DrawLine (transform.position, hit.point, Color.cyan);
+			print (hit.distance);
+			if(hit.distance < 1.5 && hit.distance > 0){
+				return true;
+			}
+		}	
+		return false;
+	}
+}
